688f775d6242cf4abbb2aa5110fd65343bb63d56
keithzeng <shihezeng@gmail.com>
2019-01-24 21:49:20 -0800

    rebuilding site Thu Jan 24 21:49:20 PST 2019

--- content/posts/my-first-post.md
+++ content/posts/my-first-post.md
@@ -4,4 +4,4 @@ date: 2019-01-24T16:32:01-08:00
 draft: false
 ---
 
-hello
+Hello, this is the first post
--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,16 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+msg="rebuilding site `date`"
+git add --all
+git commit -m "$msg"
+git push origin master
+
 # Go To Public folder
 cd public
 # Add changes to git.
-git add --all
-
+git add .
 # Commit changes.
-msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
